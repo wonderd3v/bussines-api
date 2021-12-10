35721166926c186d0f647432f8d91af0bfe2d307
wonderv <mejiahuascar@gmail.com>
2021-12-10 17:48:34 -0400

    Lo que sea 2008

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,6 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import { mongodb } from "./config/database"; 
 
-
 //Enviroment
 dotenv.config();
 
@@ -18,7 +17,9 @@ app.set('port', process.env.PORT);
 
 // Middleawaeres
 app.use(cors());
-app.use(express.json());
+app.use(express.json({
+    limit: '10mb'
+}));
 
 
 // Routes
